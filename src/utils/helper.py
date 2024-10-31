cbd06cc2908505f88880f41ff2ccdc9fbf43b898
ofplarsen <ofplarsen@gmail.com>
2024-10-31 15:20:12 +0100

    Added beta schedueler

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -21,6 +21,15 @@ def make_beta_schedule(schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2
     elif schedule == "sigmoid":
         betas = torch.linspace(-6, 6, n_timesteps, device=device)
         betas = torch.sigmoid(betas) * (end - start) + start
+    elif schedule == "cosine":
+        # Cosine schedule from DDPM++
+        s = 0.004  # Small constant to adjust the starting point
+        steps = torch.arange(n_timesteps + 1, device=device, dtype=torch.float64)
+        alphas = torch.cos(((steps / n_timesteps) + s) / (1 + s) * torch.pi / 2) ** 2
+        alphas = alphas / alphas[0]  # Normalize to ensure alphas[0] = 1
+        betas = 1 - (alphas[1:] / alphas[:-1])  # Derive beta_t from alpha_t values
+        betas = torch.clip(betas, start, end)  # Ensure betas are in the [start, end] range
+
     return betas
 
 def load_config(config_file):
