e3df328f7138e7b81cd1e836eab5a0feff243cc9
ofplarsen <ofplarsen@gmail.com>
2024-11-25 14:45:22 +0100

    Removed print from preprocess

--- src/merge_datasets.py
+++ src/merge_datasets.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     #directory = "./data"
     
     # Desired target length for all trajectories
-    target_length = 600
+    target_length = 400
     filter_alt = True
     # Change as per your requirements
     
