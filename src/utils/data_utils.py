a14d4432aaebbaf8e5a5b75091f4c040246367f0
ofplarsen <ofplarsen@gmail.com>
2025-02-27 15:23:05 +0100

    Fixed bug

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -418,10 +418,10 @@ class TrafficDataset(Dataset):
         data = data.reshape(-1, 200, len(self.features))
 
         # Restore latitude
-        data[idx, :, 0] = data[idx, :, 0] + self.lat_refs[idx]
+        data[:, :, 0] = data[idx, :, 0] + self.lat_refs[idx]
 
         # Restore longitude with scaling correction
-        data[idx, :, 1] = data[idx, :, 1] / torch.cos(torch.deg2rad(self.lat_refs[idx])) + self.lon_refs[idx]
+        data[:, :, 1] = data[idx, :, 1] / torch.cos(torch.deg2rad(self.lat_refs[idx])) + self.lon_refs[idx]
 
         return data.reshape(shape)
 
